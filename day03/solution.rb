c6588e38800c4267e1769214c5bda5abf3a36a52
Chris Roby <christopher.roby@gmail.com>
2020-12-04 15:34:34 -0800

    Day03_pt2.rb

--- day03/solution.rb
+++ day03/solution.rb
@@ -6,11 +6,9 @@ lines = input.readlines.map(&:chomp)
 line_len = lines[0].length
 total_lines = lines.length
 
-trees = 0
-
 slopes = [
 	{path: [1, 1], trees: 0},
-	{path: [3, 1], trees: 0},
+	{path: [3, 1], trees: 0}, # 268
 	{path: [5, 1], trees: 0},
 	{path: [7, 1], trees: 0},
 	{path: [1, 2], trees: 0}
@@ -19,14 +17,15 @@ slopes = [
 slopes.each do |slope|
 	x = 1
 	y = 1
-	while y + slope[:path][1] <= total_lines - 1
+	trees = 0
+
+	while y + slope[:path][1] <= total_lines
 		x += slope[:path][0]
 		idx = (x-1)%line_len
 
 		y += slope[:path][1]
 		line = lines[y-1]
 
-		puts "line #{y}, row index #{idx}, #{line[idx] == "#"}"
 		if line[idx] == "#"
 			trees +=1
 		end
@@ -34,5 +33,4 @@ slopes.each do |slope|
 	slope[:trees] = trees
 end
 
-puts slopes
 puts slopes.inject(1) {|product, s| product * s[:trees]}
