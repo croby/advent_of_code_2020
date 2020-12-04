8db71c75062a7437a4dd786f93730c18bff95de2
Chris Roby <christopher.roby@gmail.com>
2020-12-04 14:47:45 -0800

    Day02_pt2.rb

--- day02/solution.rb
+++ day02/solution.rb
@@ -3,15 +3,25 @@
 input = File.open("input.txt")
 lines = input.readlines.map(&:chomp)
 
+def score_line(pw, policy_char, *idx)
+	score = 0
+	idx.each do |i|
+		score += pw[i-1] == policy_char ? 1 : 0
+	end
+	score == 1
+end
+
 valid = 0
 
 lines.each do |line|
 	policy, pw = line.split(": ")
-	lower_bound = policy.split("-")[0].to_i
-	upper_bound = policy.split("-")[1].split(" ")[0].to_i
+	first_idx = policy.split("-")[0].to_i
+	second_idx = policy.split("-")[1].split(" ")[0].to_i
 	policy_char = policy.split(" ")[1]
 
-	if pw.count(policy_char).between?(lower_bound, upper_bound)
+	if score_line(pw, policy_char, first_idx, second_idx)
 		valid += 1
 	end
 end
+
+puts valid
